3e659de1d3cc0318759bf09b70cb5b9ee7eddb53
kabrendel <kbrend1@wgu.edu>
2022-09-04 12:27:28 -0400

    Moving data collection objects.

---
+++ Appointment Manager/DBObjects.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appointment_Manager
+{
+    internal class DBObjects
+    {
+        //  Collections for database objects.
+        public BindingList<Address> Addresses;
+        public BindingList<Appointment> Appointments;
+        public BindingList<City> Cities;
+        public BindingList<Country> Countries;
+        public BindingList<Customer> Customers;
+        public BindingList<User> Users;
+        //
+
+    }//  End of Class
+}
--- Appointment Manager/Main.cs
+++ Appointment Manager/Main.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Linq;
 using System.Windows.Forms;
-using MySql.Data.MySqlClient;
 
 namespace Appointment_Manager
 {
